a898ea2ab0fc0bb41ba49cc298e849ee56c0df09
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-03-28 01:19:36 +0000

    added full page background image of analog wall clock off center of forms  to right

--- modifyEmployee.js
+++ modifyEmployee.js
@@ -1,4 +1,4 @@
-	function makePOSTRequest() {
+	function makeModifyRequest() {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
